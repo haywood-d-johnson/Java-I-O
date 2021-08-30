0dcfde461889cafc0a5f3c3db92ce447d042ec37
haywood-d-johnson <haywood.d.johnson@gmail.com>
2021-08-29 20:43:24 -0500

    feat: added text files and tested existence

--- src/main/java/com/orb/smartlogiccodetest/Main.java
+++ src/main/java/com/orb/smartlogiccodetest/Main.java
@@ -11,11 +11,6 @@ import java.io.IOException;
 
 public class Main {
     
-    //@Test
-    public void testImport() throws Throwable {
-    
-    }
-    
     public static void main(String[] args) throws IOException{
         
     }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,11 +1,24 @@
 
-import static org.junit.Assert.assertEquals;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import org.junit.Test;
 
 public class MainTest {
 
     @Test
     public void testIfFileExists() {
-        
+        //Tests if txt files exist; Preimptive IOEXception 
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("src/resources/comma.txt"));
+            System.out.println("Testing Assertion...");
+            String line;
+            while ((line = br.readLine()) != null) {
+                assert line != null;
+                break;
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
